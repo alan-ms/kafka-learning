47b03a5dbb7a5518943c71926be55a147139773c
Alan Marques Sousa <alan.sousa@protonmail.com>
2024-08-15 19:12:02 -0300

    Implementação do consumer kafka das mudanças recentes da wikimedia usando Spring Kafka para envio pro opensearch

---
+++ kafka-beginner/opensearch-consumer-spring/src/main/kotlin/com/alanms/demos/opensearch/spring/OpenSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.alanms.demos.opensearch.spring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpensearchconsumerApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpensearchconsumerApplication>(*args)
+}
---
+++ kafka-beginner/opensearch-consumer-spring/src/test/kotlin/com/alanms/demos/opensearch/spring/OpensearchconsumerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alanms.demos.opensearch.spring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpensearchconsumerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
