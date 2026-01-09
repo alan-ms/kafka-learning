407243e4e3719ae0322e87bcc02a118ea560843d
Alan Marques Sousa <alan.sousa@protonmail.com>
2026-01-08 22:31:41 -0300

    Implementação do kafka connect source github issues

---
+++ kafka-connect-github/src/main/java/br/com/alanms/GitHubSourceConnector.java
@@ -0,0 +1,50 @@
+package br.com.alanms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.source.SourceConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GitHubSourceConnector extends SourceConnector {
+  private static Logger log = LoggerFactory.getLogger(GitHubSourceConnector.class);
+  private GitHubSourceConnectorConfig config;
+
+  @Override
+  public String version() {
+    return VersionUtil.getVersion();
+  }
+
+  @Override
+  public void start(Map<String, String> map) {
+    config = new GitHubSourceConnectorConfig(map);
+  }
+
+  @Override
+  public Class<? extends Task> taskClass() {
+    //TODO: Return your task implementation.
+    return GItHubSourceTask.class;
+  }
+
+  @Override
+  public List<Map<String, String>> taskConfigs(int i) {
+      List<Map<String, String>> configs = new ArrayList<>(1);
+      configs.add(config.originalsStrings());
+      return configs;
+  }
+
+  @Override
+  public void stop() {
+      // Do things that are necessary to stop your connector.
+      // nothing is necessary to stop for this connector
+  }
+
+  @Override
+  public ConfigDef config() {
+    return GitHubSourceConnectorConfig.conf();
+  }
+}
---
+++ kafka-connect-github/src/test/java/br/com/alanms/GitHubSourceConnectorTest.java
@@ -0,0 +1,34 @@
+package br.com.alanms;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static br.com.alanms.GitHubSourceConnectorConfig.BATCH_SIZE_CONFIG;
+import static br.com.alanms.GitHubSourceConnectorConfig.OWNER_CONFIG;
+import static br.com.alanms.GitHubSourceConnectorConfig.REPO_CONFIG;
+import static br.com.alanms.GitHubSourceConnectorConfig.SINCE_CONFIG;
+import static br.com.alanms.GitHubSourceConnectorConfig.TOPIC_CONFIG;
+import static org.junit.Assert.assertEquals;
+
+public class GitHubSourceConnectorTest {
+
+    private Map<String, String> initialConfig() {
+        Map<String, String> baseProps = new HashMap<>();
+        baseProps.put(OWNER_CONFIG, "foo");
+        baseProps.put(REPO_CONFIG, "bar");
+        baseProps.put(SINCE_CONFIG, "2017-04-26T01:23:45Z");
+        baseProps.put(BATCH_SIZE_CONFIG, "100");
+        baseProps.put(TOPIC_CONFIG, "github-issues");
+        return (baseProps);
+    }
+
+    @Test
+    public void taskConfigsShouldReturnOneTaskConfig() {
+        GitHubSourceConnector gitHubSourceConnector = new GitHubSourceConnector();
+        gitHubSourceConnector.start(initialConfig());
+        assertEquals(gitHubSourceConnector.taskConfigs(1).size(),1);
+        assertEquals(gitHubSourceConnector.taskConfigs(10).size(),1);
+    }
+}
